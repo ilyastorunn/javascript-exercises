7817e88c011ab317b3048dd35aefda1a4ae45dfc
İlyas Torun <ilyastorun@192.168.1.7>
2024-10-10 22:01:00 +0300

    fix: Resolve issues 8 to 11.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function(str) {
+    let cleanString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let palindrome = cleanString.split('').reverse().join('');
+    return cleanString === palindrome;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    let fib0 = 0;
+    let fib1 = 1;
 
+    if(n < 0) {
+        return "OOPS";
+    }
+
+    for(let i = 2; i <= n; i++){
+        let nextFib = fib0 + fib1;
+        fib0 = fib1;
+        fib1 = nextFib;
+    }
+    return (n === 0) ? fib0 : fib1;
 };
 
 // Do not edit below this line
